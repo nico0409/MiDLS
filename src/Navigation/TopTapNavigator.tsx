ec69d0bc1286210d1f95a77fb0ea658bb831ea71
Alan <alan.ch1@hotmail.com>
2021-07-15 20:25:44 -0300

    +ToggleHeaderDrawer, to TopTabNav

--- src/Navigation/TopTapNavigator.tsx
+++ src/Navigation/TopTapNavigator.tsx
@@ -1,27 +1,34 @@
 import React from 'react'
-import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
 import { SafeAreaView, StyleSheet } from 'react-native';
 
-
+import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
+import { DrawerScreenProps } from '@react-navigation/drawer';
 
 import { MyProfileScreen } from '../screens/MyProfileScreen';
 import { MapScreen } from '../screens/MapScreen';
 import { NewsScreen } from '../screens/NewsScreen';
+import { ToggleDrawerHeader } from '../components/ToggleDrawerHeader';
 
+interface Props extends DrawerScreenProps<any, any> { };
 
 const Tab = createMaterialTopTabNavigator();
 
-export const TopTapNavigator=()=> {
+export const TopTapNavigator = ({ navigation, route }: Props) => {
   return (
     <SafeAreaView style={styles.container}>
-    <Tab.Navigator>
-      <Tab.Screen name="NewScreen" component={NewsScreen} />
-      <Tab.Screen name="MapScreen" component={MapScreen} /> 
-      <Tab.Screen name="MyProfileScreen" component={MyProfileScreen} /> 
-    </Tab.Navigator>
+
+      <ToggleDrawerHeader navigation={navigation} route={route} />
+
+      <Tab.Navigator>
+        <Tab.Screen name="NewScreen" component={NewsScreen} />
+        <Tab.Screen name="MapScreen" component={MapScreen} />
+        <Tab.Screen name="MyProfileScreen" component={MyProfileScreen} />
+      </Tab.Navigator>
+
     </SafeAreaView>
   );
 }
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
