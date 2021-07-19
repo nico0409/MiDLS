ff9dcda9cbedf5ee33acdd99ba2d09bda64a30b3
Alan <alan.ch1@hotmail.com>
2021-07-19 15:14:09 -0300

    Pantalla redes, reanimated 1.4.0
    hace pod install

--- src/components/DrawerMenu.tsx
+++ src/components/DrawerMenu.tsx
@@ -1,10 +1,72 @@
-import React from 'react'
-import { Text, View } from 'react-native';
+import React, { useState } from 'react'
+import { Text, View, TouchableOpacity, StyleSheet, Modal } from 'react-native';
+import { RedesContent } from './RedesContent';
 
 export const DrawerMenu = () => {
+
+    const [isVisible, setIsVisible] = useState(false);
+
     return (
-        <View>
-            <Text>Drawer Menu</Text>
+        <View style={styles.container}>
+
+            <TouchableOpacity
+                style={styles.botonRedes}
+                onPress={() => setIsVisible(true)}>
+                <Text>Redes</Text>
+            </TouchableOpacity>
+
+            <Modal
+                animationType="slide"
+                visible={isVisible}
+                transparent={true}
+            >
+                <View style={styles.modalContainer}>
+                    <View style={styles.modalBackground}>
+
+                        <RedesContent />
+
+                        <TouchableOpacity
+                            style={styles.botonRedesClose}
+                            onPress={() => setIsVisible(false)}>
+                            <Text>Cerrar Ventana</Text>
+                        </TouchableOpacity>
+
+                    </View>
+                </View>
+            </Modal>
+
         </View>
     )
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        alignItems: 'center',
+    },
+    botonRedes: {
+        width: '80%',
+        alignItems: 'center',
+        justifyContent: 'center',
+        borderWidth: 2
+    },
+    modalContainer: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    modalBackground:{
+        backgroundColor: 'white',
+        height: '90%',
+        width: '90%',
+        borderWidth: 2
+    },
+    botonRedesClose:{
+        width: '80%',
+        alignSelf: 'center',
+        alignItems: 'center',
+        justifyContent: 'center',
+        borderWidth: 2,
+        marginVertical: 15
+    }
+})
---
+++ src/data/listRedes.tsx
@@ -0,0 +1,64 @@
+import { DataRedes } from '../interface/PropsRedes';
+
+export const listRedes: DataRedes[] = [
+    {
+        id: 1,
+        props: {
+            type: "icon",
+            nameOrUrl: "logo-youtube",
+            color: "black",
+            size: 80,
+            descr: "dls-archer.com"
+        }
+    },
+    {
+        id: 2,
+        props: {
+            type: "icon",
+            nameOrUrl: "logo-youtube",
+            color: "black",
+            size: 80,
+            descr: "archerwell.com"
+        }
+    },
+    {
+        id: 3,
+        props: {
+            type: "icon",
+            nameOrUrl: "logo-youtube",
+            color: "black",
+            size: 80,
+            descr: "DLS Latinoamerica"
+        }
+    },
+    {
+        id: 4,
+        props: {
+            type: "icon",
+            nameOrUrl: "logo-youtube",
+            color: "black",
+            size: 80,
+            descr: "Archer Well"
+        }
+    },
+    {
+        id: 5,
+        props: {
+            type: "icon",
+            nameOrUrl: "logo-linkedin",
+            color: "black",
+            size: 80,
+            descr: "DLS Archer"
+        }
+    },
+    {
+        id: 6,
+        props: {
+            type: "icon",
+            nameOrUrl: "logo-linkedin",
+            color: "black",
+            size: 80,
+            descr: "DLS Argentina"
+        }
+    },
+]
