dfd2bb586f0a18c2318f03c07c684e50a85d480a
Alan <alan.ch1@hotmail.com>
2021-10-04 20:05:11 -0300

    Componente PickerSelect Mejorado

--- src/components/CreateObservePageTwo.tsx
+++ src/components/CreateObservePageTwo.tsx
@@ -4,29 +4,22 @@ import { colors } from '../Themes/DlsTheme';
 import { PickerSelect } from './PickerSelect';
 
 export const CreateObservePageTwo = () => {
-    const deportes = [
-        { label: 'Football', value: 'football' },
-        { label: 'Baseball', value: 'baseball' },
-        { label: 'Hockey', value: 'hockey' },
-        { label: 'Basket', value: 'basket' },
-    ]
-
     const [isEnabled, setIsEnabled] = useState(false);
 
     const toggleSwitch = () => {
-        setIsEnabled( !isEnabled );
+        setIsEnabled(!isEnabled);
         //onChange( !isEnabled );
     };
 
     return (
         <View>
-            <PickerSelect placeholder="Puesto" item={deportes} />
-        <Switch
-            trackColor={{ false: 'gray', true: colors.dlsYellowSecondary}}
-            thumbColor={ (Platform.OS === 'android' ) ? colors.dlsYellowSecondary : '' }
-            onValueChange={ toggleSwitch }
-            value={ isEnabled }
-        />
+            <PickerSelect placeholder="Puesto" type={"DLHR_PUESTO"} />
+            <Switch
+                trackColor={{ false: 'gray', true: colors.dlsYellowSecondary }}
+                thumbColor={(Platform.OS === 'android') ? colors.dlsYellowSecondary : ''}
+                onValueChange={toggleSwitch}
+                value={isEnabled}
+            />
         </View>
     )
 }
--- src/components/PickerSelect.tsx
+++ src/components/PickerSelect.tsx
@@ -1,141 +1,85 @@
-import React from 'react'
-import { View } from 'react-native'
+import React, { useState } from 'react';
 import RNSingleSelect, {
     ISingleSelectDataType,
 } from "@freakycoder/react-native-single-select";
-import { StatusBar, SafeAreaView, Dimensions } from "react-native";
-
-const { width: ScreenWidth } = Dimensions.get("window");
-
-const staticData: Array<ISingleSelectDataType> = [
-    {
-        id: 0,
-        value: "Euismod Justo",
-        imageSource: require("../assets/money.png"),
-    },
-    {
-        id: 1,
-        value: "Risus Venenatis",
-        imageSource: require("../assets/beer.png"),
-    },
-    {
-        id: 2,
-        value: "Vestibulum Ullamcorper",
-        imageSource: require("../assets/party.png"),
-    },
-    {
-        id: 3,
-        value: "Lorem Nibh",
-        imageSource: require("../assets/food-and-restaurant.png"),
-    },
-    {
-        id: 4,
-        value: "Ligula Amet",
-        imageSource: require("../assets/guitar.png"),
-    },
-];
-
-
-export const PickerSelect = () => {
-    const [dynamicData, setDynamicData] = React.useState<
-        Array<ISingleSelectDataType>
-    >([]);
-
-    React.useEffect(() => {
-        setTimeout(() => {
-            setDynamicData(staticData);
-        }, 2000);
-    });
-    return (
-
-        <View
-            style={{
-                
-                //backgroundColor: "#454851",
-                 backgroundColor: "#eceef3",
-                height:200,
-                justifyContent: "center",
-            }}>
-            <View
-                style={{
-                    shadowRadius: 12,
-                    shadowOpacity: 0.1,
-                    shadowColor: "#757575",
-                    shadowOffset: {
-                        width: 0,
-                        height: 3,
-                    },
-                }}
-            >
-                <RNSingleSelect
-                    darkMode
-                    data={dynamicData}
-                    width={ScreenWidth * 0.9}
-                    searchEnabled={false}
-                    menuBarContainerWidth={ScreenWidth * 0.9}
-                    onSelect={(selectedItem: ISingleSelectDataType) =>
-                        console.log("SelectedItem: ", selectedItem)
-                    }
-                />
-                 <RNSingleSelect
-                    darkMode
-                    data={dynamicData}
-                    width={ScreenWidth * 0.9}
-                    searchEnabled={false}
-                    menuBarContainerWidth={ScreenWidth * 0.9}
-                    onSelect={(selectedItem: ISingleSelectDataType) =>
-                        console.log("SelectedItem: ", selectedItem)
-                    }
-                />
-            </View>
-        </View>
-    )
-}
-
-/* import React, { useState } from 'react'
-import { View, Text, Animated } from 'react-native';
-import RNPickerSelect from 'react-native-picker-select';
+import { Dimensions, View } from 'react-native';
+import { promptType, DlhrBussinesUnit, DlhrOrigen, DlhrPuesto, DlhrTurno } from '../interfaces/prompInterfaces';
+import { GetPromptArray } from './GetPromptArrayy';
 
 interface Props {
     placeholder: string;
-    item: item[]
-    itmeSelect?:number
+    type: "DLHR_EMPL_BUSSINES_UNIT" | "DLHR_TURNO" | "DLHR_ORIGEN" | "DLHR_PUESTO";
+    itemSelect?: number;
 }
 
-interface item {
-    label: string ;
-    value: string;
+interface DlhrPromptsDet extends DlhrOrigen, DlhrPuesto, DlhrTurno, DlhrBussinesUnit { };
+
+const { width: ScreenWidth } = Dimensions.get("window");
+
+export const PickerSelect = ({ placeholder, type }: Props) => {
+
+    const [selectedItem, setSelectedItem] = useState<ISingleSelectDataType>()
+
+    const promptType: promptType = { type };
+
+    const { PromptObArray } = GetPromptArray(promptType);
+
+    let data: ISingleSelectDataType[];
+
+    if (PromptObArray[0] !== undefined) {
+
+        let allArrayC: DlhrPromptsDet[];
+
+        /* Unidad de Negocio */
+        if (type === "DLHR_EMPL_BUSSINES_UNIT") {
+            const businessUArrayC: any = PromptObArray.filter(
+                item => item.DLHR_OBSERVE_EMPLID?.EMPLID == 'C020513'
+            )[0].DLHR_BUSSINES_UNIT;
+            allArrayC = Array.isArray(businessUArrayC) ? businessUArrayC : [businessUArrayC];
+        } else {
+            allArrayC = Array.isArray(PromptObArray) ? PromptObArray : [PromptObArray];
+        }
+
+        data = allArrayC!.map(
+            (item, index) => {
+                let dataItem;
+                switch (type) {
+                    case "DLHR_ORIGEN":
+                        dataItem = item.ORIGEN;
+                        break;
+                    case "DLHR_PUESTO":
+                        dataItem = item.DL_PUESTO;
+                        break;
+                    case "DLHR_TURNO":
+                        dataItem = item.DL_TURNO;
+                        break;
+                    case "DLHR_EMPL_BUSSINES_UNIT":
+                        dataItem = item.UNIDAD_DE_NEGOCIO;
+                        break;
+                }
+                return { id: index, value: item.DESCR, data: dataItem }
+            }
+        )
+
+    } else {
+        data = [];
+    }
+
+    return (
+        <View>
+            <RNSingleSelect
+                darkMode
+                initialValue={selectedItem}
+                placeholder={placeholder}
+                data={data}
+                width={ScreenWidth * 0.9}
+                searchEnabled={false}
+                menuBarContainerWidth={ScreenWidth * 0.9}
+                onSelect={(selectedItem: ISingleSelectDataType) => {
+                    console.log("SelectedItem: ", selectedItem);
+                    setSelectedItem(selectedItem);
+                }}
+            />
+        </View>
+    )
 }
-
-export const PickerSelect = ({ placeholder, item }: Props) => {
-
-    const [state, setstate] = useState({
-        label: placeholder,
-        value: null,
-        color: 'blue',
-})
-
-const deportes=[
-    { label: 'Football', value: 'football' },
-    { label: 'Baseball', value: 'baseball' },
-    { label: 'Hockey', value: 'hockey' },
-]
-
-
-console.log('test4',state);
-
-return (
-    <View style={{}}>
-        <RNPickerSelect
-        onValueChange={(value) => setstate({...state,value,label:deportes.find(function(element){return element.value===value})?.label!})}
-        placeholder={{ label: state.label,
-            value: state.value,
-            color: 'blue', }}
-        items={deportes}
-
-    />
-      <Text style={{width: '100%', height: 60, position: 'absolute', bottom: 0, left: 0}}>{' '}</Text>
-    </View>
-)
-}
- */
