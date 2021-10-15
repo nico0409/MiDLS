74fa4ebd79c3c27852596023edefe5e9acac221f
Alan <alan.ch1@hotmail.com>
2021-10-15 10:54:58 -0300

    se saca comentario

--- src/components/FadeQuestionsScreen.tsx
+++ src/components/FadeQuestionsScreen.tsx
@@ -12,77 +12,9 @@ interface Props {
     carouselRef?: React.MutableRefObject<null>
 }
 
-/* type Props2 = {
-    children: React.ReactElement;
-    waitBeforeShow?: number;
-};
- */
-
 const windowWidth = Dimensions.get('window').width;
 
 export const FadeQuestionsScreen = ({ animatedValueOp,animatedValue, dataCarousel, carouselRef }: Props) => {
-
-
-    /* const ScreensCarousel = (indexSI: number) => {
-
-        switch (indexSI) {
-            case 2:
-                return (
-                    <View >
-                        <CarouselForQuestions data={dataCarousel.find(({ index }) => index === indexSI)!.questions} />
-                    </View>
-                )
-            case 3:
-                return (
-                    <View >
-                        <CarouselForQuestions data={dataCarousel.find(({ index }) => index === indexSI)!.questions} />
-                    </View>
-                )
-            case 4:
-                return (
-                    <View >
-                        <CarouselForQuestions data={dataCarousel.find(({ index }) => index === indexSI)!.questions} />
-                    </View>
-                )
-        }
-    }; */
-
-    /*     const Delayed = ({ children, waitBeforeShow = 100 }: Props2) => {
-            const [isShown, setIsShown] = useState(false);
-    
-            useEffect(() => {
-                console.log("se ejecuta delayed");
-                console.log(waitBeforeShow);
-                setTimeout(() => {
-                    setIsShown(true);
-                }, waitBeforeShow);
-            }, []);
-    
-            return isShown ? children : null;
-        }; */
-
-
-    /* const scrollX = useRef(new Animated.Value(0)).current; */
-
-    const CarouselList = () => {
-
-        return (<>
-            {
-                dataCarousel.map(element => {
-                    return (
-                        <View
-                            style={{ width: windowWidth }}
-                            key={element.index}
-                        >
-                            <CarouselForQuestions data={element.questions} />
-                        </View>
-                    )
-                })
-            }
-        </>)
-
-
-    }
 
     return (
         <>
@@ -106,43 +38,14 @@ export const FadeQuestionsScreen = ({ animatedValueOp,animatedValue, dataCarouse
                         return (
                             <View
                                 style={{ width: windowWidth}}
+                                key={element.index}
                             >
                                 <CarouselForQuestions data={element.questions} />
                             </View>
                         )
                     })
                 }
-                {/* <ScrollView
-                    horizontal={false}
-                    pagingEnabled
-                    scrollEnabled={false}
-                    showsHorizontalScrollIndicator={false}
-                    
-                    onScroll={Animated.event([
-                        {
-                            nativeEvent: {
-                                contentOffset: {
-                                    x: scrollX
-                                }
-                            }
-                        }
-                    ],
-                        { useNativeDriver: false })}
-                    scrollEventThrottle={1}
-                >
-                    {dataCarousel.map(element => {
-                        return (
-                            <View
-                                style={{flex:1, width: windowWidth }}
-                                key={element.index}
-                            >
-                                <CarouselForQuestions data={element.questions} />
-                            </View>
-                        )
-                    })}
-                </ScrollView> */}
 
-                {/* {ScreensCarousel(index)} */}
             </Animated.View>
         </>
     )
