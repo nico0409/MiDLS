104b1e75cf435943f768580352c90765fdce4df1
Alan <alan.ch1@hotmail.com>
2021-11-11 23:12:57 -0300

    fix primise final page obs

--- src/components/Questions.tsx
+++ src/components/Questions.tsx
@@ -9,15 +9,13 @@ import BouncyCheckboxGroup, {
 import { QuestionsData } from '../data/QuestionsData';
 import { Questions, questionType } from "../interfaces/QuestionInterfaces";
 
-
-
-
 interface Props {
   form?: M38GetCompIntfcDLHRTAOBSERVCIResponse
   questiontType: questionType
   onChange: (value: string, field: keyof objUseForm) => void;
   darkText?: boolean;
 }
+
 export const QuestionsCmp = ({ form, questiontType, onChange, darkText = false }: Props) => {
 
   let initialValue = 0
@@ -32,16 +30,12 @@ export const QuestionsCmp = ({ form, questiontType, onChange, darkText = false }
         return 2
       case 'D':
         return 3
-
       default:
         return 4
     }
-
-
   }
 
   const mapIndexToValue = (index: number) => {
-
     switch (index) {
       case 0:
         return 'A'
@@ -51,24 +45,21 @@ export const QuestionsCmp = ({ form, questiontType, onChange, darkText = false }
         return 'C'
       case 3:
         return 'D'
-
       default:
         return 'A'
     }
   }
 
-
   const data = QuestionsData.type.filter(item => {
     if (item.type.type === questiontType.type)
-    return item;
-
+      return item;
   })
 
   data[0]?.choices.map(element => {
     element.textStyle = {
       textDecorationLine: "none",
       color: darkText ? 'black' : 'white',
-      fontSize:18
+      fontSize: 16
     }
   });
 
@@ -79,19 +70,17 @@ export const QuestionsCmp = ({ form, questiontType, onChange, darkText = false }
     initialValue = mapValuesToIndex()
   }
 
-
   return (
     <>
       <SafeAreaView>
-
         <View style={styles.container}>
-
 
           <View style={{ /* marginLeft: 32, */ marginTop: 24 }}>
             <Text style={{ color: darkText ? 'black' : 'white', fontWeight: "bold", fontSize: 22 }}>
               {data[0]?.question}
             </Text>
           </View>
+
           <View
             style={{
               marginTop: 16,
@@ -114,7 +103,6 @@ export const QuestionsCmp = ({ form, questiontType, onChange, darkText = false }
     </>
   );
 };
-
 
 const styles = StyleSheet.create({
   container: {
