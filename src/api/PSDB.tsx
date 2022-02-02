4aeecbc8477ed532f0a0c65bff5594016f425c46
Nicolas Gonzalez <nico04093@gmail.com>
2022-02-02 19:00:43 -0300

    add timeout, builds android ios

--- src/api/PSDB.tsx
+++ src/api/PSDB.tsx
@@ -1,7 +1,9 @@
 import axios from 'axios';
+import { Platform } from 'react-native';
 
 const PSDB = axios.create({
-    baseURL: 'http://www.dls-tst-peoplesoft.com:37600/PSIGW/PeopleSoftServiceListeningConnector'
+    baseURL: 'http://www.dls-tst-peoplesoft.com:37600/PSIGW/PeopleSoftServiceListeningConnector',
+    timeout: Platform.OS === "ios" ? 19000 : 60000 
 });
 
 export default PSDB;
