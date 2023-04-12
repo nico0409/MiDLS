1fc68ac4b32b61882489d7efb381e332327b66cf
Nicolas Gonzalez <nico04093@gmail.com>
2023-04-12 10:55:10 -0300

    background process v1

--- src/context/AuthContext.tsx
+++ src/context/AuthContext.tsx
@@ -2,9 +2,14 @@ import { useNetInfo } from '@react-native-community/netinfo'
 import React, { createContext, useReducer, useState, useEffect } from 'react'
 import { authReducer, AuthState } from './authReducer'
 import { SendObserveStorage } from '../components/SendObserveStorage';
-import { Asingstorage } from '../components/Storage';
+import { Asingstorage, GetStorage } from '../components/Storage';
 import { StorageTypes } from '../interfaces/prompInterfaces';
 import { GetPrompt } from '../components/GetPrompt';
+
+import BackgroundTimer from 'react-native-background-timer';
+import { log } from 'react-native-reanimated';
+import { useAllObserve } from '../hooks/useAllObserve';
+import { storageEmplid } from '../interfaces/storageInterface';
 
 
 
@@ -102,15 +107,42 @@ export const AuthProvider = ({ children }: any) => {
 
     const [isErrorResponse,setIsErrorResponse] = useState(false);
     
-    const timeOutAction = async() =>{
+    const [emplid, setEmplid] = useState('');
+    const { ErrorResponse, loadAllObserve } = useAllObserve(emplid);
+    
+    const getEmplid = async() =>{
+            function valEmplid(object: any): object is storageEmplid {
+                return true
+            }
+
+            const getemplid = await GetStorage({ StorageType: 'emplid' });
+            if (getemplid !== null) {
+                if (valEmplid(getemplid)) {
+                    setEmplid(getemplid.emplid);
+                }
+            }
+    }
+
+    const refreshData = async() =>{
         const prompts: StorageTypes = { StorageType: 'prompt' };
 
+        await getEmplid();
+
         await Asingstorage(prompts, await GetPrompt(setIsErrorResponse));
-        console.log("time out ejecutado-------");
+
+        if (emplid) {
+            await loadAllObserve();
+            await SendObserveStorage();
+        }
         
     };
 
     useEffect(()=>{
+        BackgroundTimer.runBackgroundTimer(() => { 
+            console.log("se ejecuto background timer--------------------------------");
+            refreshData();
+            }, 
+            3600000);
         /* setTimeout(timeOutAction,120000); */
     },[]);
 
