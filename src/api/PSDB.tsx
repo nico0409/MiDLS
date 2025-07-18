42b7deca885a3abac27d2b207aaf0dae6490680a
Nicolas Gonzalez <nico04093@gmail.com>
2025-07-18 16:17:38 -0300

    build 102 dev 1.13.2

--- src/api/PSDB.tsx
+++ src/api/PSDB.tsx
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const PSDB = axios.create({
     baseURL: 'http://10.0.10.251:27600/PSIGW/PeopleSoftServiceListeningConnector',
-    timeout: 5000 
+    timeout: 30000 
 });
 
 export default PSDB;
